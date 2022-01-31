1cb28489a3693e51852852dde78cfc6dbf253b1f
ahmetboydas <info@ahmetboydas.com.tr>
2022-01-31 14:31:20 +0300

    Initial commit

---
+++ app/Controllers/Backend/Auth.php
@@ -0,0 +1,23 @@
+<?php 
+namespace App\Controllers\Backend;
+use CodeIgniter\BaseController;
+
+class Auth extends BaseController{
+	public function login()
+	{
+		echo "Giriş Ekranı";
+	}
+
+	public function logout()
+	{
+		echo "Çıkış Ekranı";
+	}
+}
+
+
+
+
+
+
+
+ ?>
